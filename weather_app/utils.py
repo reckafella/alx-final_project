c353dfebed7fe95b6ed0d7674dddbbc40f339465
Reckafella <ethanmuthoni@gmail.com>
2024-03-02 22:16:30 +0300

    Add caching in Redis

--- weather_app/utils.py
+++ weather_app/utils.py
@@ -1,18 +1,29 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
 from django.conf import settings
+import redis
+import json
 
 
 class WeatherAPI:
     def __init__(self):
         self.api_key = settings.WEATHER_API_KEY
         self.base_url = 'https://api.weatherapi.com/v1/{}?key={}&q={}'
+        self.redis_client = redis.StrictRedis(host='localhost',
+                                              port=6379, db=0)
 
     def get_weather(self, city: str) -> object:
         """ Fetch current weather data """
+        cached_weather_data = self.redis_client.get(f'weather:{city}')
+        if cached_weather_data:
+            return json.loads(cached_weather_data.decode('utf-8'))
+
         weather_url = self.base_url.format('current.json', self.api_key, city)
 
         response = requests.get(weather_url).json()
+
+        lt = response['location']['localtime_epoch']
+
         weather_data = {
             'city': response['location']['name'],
             'country': response['location']['country'],
@@ -23,14 +34,22 @@ class WeatherAPI:
             'wind_speed': response['current']['wind_kph'],
             'condition': response['current']['condition']['text'],
             'icon': response['current']['condition']['icon'],
-            'local_time': datetime.strptime(response['location']['localtime'], '%Y-%m-%d %H:%M'),
+            'local_time': datetime.fromtimestamp(lt).strftime('%I:%M:%S %p'),
         }
+
+        self.redis_client.setex(f'weather:{city}', timedelta(minutes=10),
+                                json.dumps(weather_data).encode('utf-8'))
 
         return weather_data
 
     def get_forecast(self, city: str) -> tuple:
         """ Fetch forecast weather data """
-        forecast_url = self.base_url.format('forecast.json', self.api_key, city)
+        cached_forecast_data = self.redis_client.get(f'forecast:{city}')
+        if cached_forecast_data:
+            return json.loads(cached_forecast_data.decode('utf-8'))
+
+        forecast_url = self.base_url.format('forecast.json',
+                                            self.api_key, city)
         forecast_url += '&days=7'
 
         response = requests.get(forecast_url).json()
@@ -45,8 +64,8 @@ class WeatherAPI:
 
         for forecast in response['forecast']['forecastday']:
             forecast_data.append({
-                'day': forecast['date'],
-                'date': datetime.strptime(forecast['date'], '%Y-%m-%d').strftime('%a'),
+                'day': datetime.strptime(forecast['date'],
+                                         '%Y-%m-%d').strftime('%a, %d %b %Y'),
                 'max_temp': forecast['day']['maxtemp_c'],
                 'min_temp': forecast['day']['mintemp_c'],
                 'condition': forecast['day']['condition']['text'],
@@ -54,5 +73,8 @@ class WeatherAPI:
                 'humidity': forecast['day']['avghumidity'],
                 'wind_speed': forecast['day']['maxwind_kph'],
             })
+
+        self.redis_client.setex(f'forecast:{city}', timedelta(hours=3),
+                                json.dumps(forecast_data).encode('utf-8'))
 
         return location, forecast_data
